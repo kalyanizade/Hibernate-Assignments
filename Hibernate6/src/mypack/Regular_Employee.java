61f65b66e0f5bb0fb16595377b28f04c54496c2f
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-02-23 17:00:06 +0530

    Add files via upload

---
+++ Hibernate6/src/mypack/Regular_Employee.java
@@ -0,0 +1,23 @@
+package mypack;
+
+public class Regular_Employee extends Employee
+{
+	private int salary,bonus;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+
+}
---
+++ Hibernate6/src/mypack/TestMain.java
@@ -0,0 +1,32 @@
+package mypack;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestMain {
+public static void main(String args[])
+{
+	Configuration cfg=new Configuration();
+	cfg.configure("hibernate.cfg.xml");
+	SessionFactory factory=cfg.buildSessionFactory();
+	Session session=factory.openSession();
+	Transaction tr=session.beginTransaction();
+	Employee e1=new Employee();
+	e1.setName("kalyani zade");
+	Regular_Employee e2=new Regular_Employee();
+	e2.setName("prachi zade");
+	e2.setSalary(45546);
+	e2.setBonus(5000);
+	Contract_Employee e3=new Contract_Employee();
+	e3.setName("Bhushan zade");
+	e3.setPay_per_hour(500);
+	e3.setContract_period("2 years");
+	session.save(e1);
+	session.save(e2);
+	session.save(e3);
+	tr.commit();
+	session.close();
+}
+}
